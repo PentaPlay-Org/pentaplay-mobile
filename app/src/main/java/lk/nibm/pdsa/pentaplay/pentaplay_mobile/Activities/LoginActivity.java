bac240d7df37f1e48c70ddabb46064e4f5fab8db
Pavithra Chamod J <84024015+Pavithra-cj@users.noreply.github.com>
2024-03-22 22:38:22 +0530

    Setup project structure.

---
+++ app/src/main/java/lk/nibm/pdsa/pentaplay/pentaplay_mobile/Activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package lk.nibm.pdsa.pentaplay.pentaplay_mobile.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import lk.nibm.pdsa.pentaplay.pentaplay_mobile.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}

1f6bae22422dc75ffa558cb6f26e33d1a15ca22c
Pavithra Chamod <pavithra@cosmoforge.com>
2024-04-06 21:41:59 +0530

    Common UIs done. FirebaseHandler function created.

--- app/src/main/java/lk/nibm/pdsa/pentaplay/pentaplay_mobile/Activities/WelcomeActivity.java
+++ app/src/main/java/lk/nibm/pdsa/pentaplay/pentaplay_mobile/Activities/WelcomeActivity.java
@@ -2,15 +2,62 @@ package lk.nibm.pdsa.pentaplay.pentaplay_mobile.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import lk.nibm.pdsa.pentaplay.pentaplay_mobile.Model.Player;
 import lk.nibm.pdsa.pentaplay.pentaplay_mobile.R;
+import lk.nibm.pdsa.pentaplay.pentaplay_mobile.databinding.ActivityWelcomeBinding;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private ActivityWelcomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        binding = ActivityWelcomeBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        setListners();
     }
+
+    private void setListners()
+    {
+
+        binding.btnEnter.setOnClickListener(v-> {
+            if (isValidSignInDetails())
+            {
+                Player player = new Player(binding.inputName.getText().toString());
+                Intent intent = new Intent(WelcomeActivity.this, GamesMenuActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void showToast(String message)
+    {
+
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+
+    }
+
+    private Boolean isValidSignInDetails()
+    {
+
+        if(binding.inputName.getText().toString().trim().isEmpty())
+        {
+            showToast("Enter Name");
+            return false;
+        }else
+        {
+            return true;
+        }
+
+    }
+
+
 }
---
+++ app/src/main/java/lk/nibm/pdsa/pentaplay/pentaplay_mobile/Model/Player.java
@@ -0,0 +1,60 @@
+package lk.nibm.pdsa.pentaplay.pentaplay_mobile.Model;
+
+import java.util.Date;
+import java.util.Map;
+
+public class Player {
+
+    private String name;
+    private Map<String, Object> board;
+    private String date;
+    private String time;
+    private String gameCode;
+
+    public Player(String playerName) {
+        this.name = playerName;
+        Date currentDate = new Date();
+        this.date = currentDate.toString().split(" ")[1] + " " + currentDate.toString().split(" ")[2] + ", " + currentDate.toString().split(" ")[5];
+        this.time = currentDate.toString().split(" ")[3];
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Map<String, Object> getBoard() {
+        return board;
+    }
+
+    public void setBoard(Map<String, Object> board) {
+        this.board = board;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getGameCode() {
+        return gameCode;
+    }
+
+    public void setGameCode(String gameCode) {
+        this.gameCode = gameCode;
+    }
+}

cc957f03b46f1211e22b1a9a9a49e0692d66b0ae
Pavithra Chamod <pavithra@cosmoforge.com>
2024-04-06 18:29:44 +0530

    Game 2 completed.

---
+++ app/src/main/java/lk/nibm/pdsa/pentaplay/pentaplay_mobile/Activities/Games/Game2_TicTacToe/Logics/Cell.java
@@ -0,0 +1,21 @@
+package lk.nibm.pdsa.pentaplay.pentaplay_mobile.Activities.Games.Game2_TicTacToe.Logics;
+
+public class Cell {
+
+    private int i;
+    private int j;
+
+    public Cell(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public int getJ() {
+        return j;
+    }
+
+}
